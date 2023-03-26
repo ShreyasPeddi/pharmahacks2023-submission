a00552ccf023cf5b45dc8303dd737ef206cb4958
Vidhi Ruparel <83102803+Vidhi-26@users.noreply.github.com>
2023-03-26 10:36:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from xgboost import XGBClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import StackingClassifier
-from sklearn.metrics import f1_score, cohen_kappa_score, classification_report
+from sklearn.metrics import accuracy_score, f1_score, cohen_kappa_score, classification_report
 
 #get training and testing data
 data = pd.read_csv('./bioassay/AID373red_train.csv')
@@ -15,10 +15,10 @@ test_data = pd.read_csv('./bioassay/AID373red_test.csv')
 data["Outcome"] = data["Outcome"].map({"Inactive": 0, "Active": 1})
 test_data["Outcome"] = test_data["Outcome"].map({"Inactive": 0, "Active": 1})
 
-x_train = data[["WBN_GC_L_A","WBN_GC_H_A","WBN_GC_L_B","WBN_GC_H_B","WBN_GC_L_C","WBN_GC_H_C","WBN_GC_L_D","WBN_GC_H_D","WBN_EN_L_A","WBN_EN_H_A","WBN_EN_L_B","WBN_EN_H_B","WBN_EN_L_C","WBN_EN_H_C","WBN_EN_L_D","WBN_EN_H_D","WBN_LP_L_A","WBN_LP_H_A","WBN_LP_L_B","WBN_LP_H_B","WBN_LP_L_C","WBN_LP_H_C","WBN_LP_L_D","WBN_LP_H_D","XLogP","PSA","NumRot","NumHBA","NumHBD","MW","BBB","BadGroup"]]
+x_train = data.drop(["Outcome"], axis=1)
 y_train = data["Outcome"]
 
-x_test = test_data[["WBN_GC_L_A","WBN_GC_H_A","WBN_GC_L_B","WBN_GC_H_B","WBN_GC_L_C","WBN_GC_H_C","WBN_GC_L_D","WBN_GC_H_D","WBN_EN_L_A","WBN_EN_H_A","WBN_EN_L_B","WBN_EN_H_B","WBN_EN_L_C","WBN_EN_H_C","WBN_EN_L_D","WBN_EN_H_D","WBN_LP_L_A","WBN_LP_H_A","WBN_LP_L_B","WBN_LP_H_B","WBN_LP_L_C","WBN_LP_H_C","WBN_LP_L_D","WBN_LP_H_D","XLogP","PSA","NumRot","NumHBA","NumHBD","MW","BBB","BadGroup"]]
+x_test = test_data.drop(["Outcome"], axis=1)
 y_test = test_data["Outcome"]
 
 #random forest model
@@ -44,14 +44,16 @@ estimators = [
 aggregate_model = StackingClassifier(estimators = estimators, 
                                     final_estimator = XGB_classifier)
 
-print(aggregate_model.fit(x_train, y_train).score(x_test, y_test))
+print("score: ", aggregate_model.fit(x_train, y_train).score(x_test, y_test))
 
 #testing metrics
 y_predictions = aggregate_model.predict(x_test)
 
 f1 = f1_score(y_test, y_predictions, average="weighted", zero_division=0)
 cohen_kappa = cohen_kappa_score(y_test, y_predictions)
+accuracy = accuracy_score(y_test, y_predictions)  
 
-print(f1)
-print(cohen_kappa)
+print("accuracy: ", accuracy)
+print("f1 score: ", f1)
+print("cohen kappa score: ", cohen_kappa)
 print(classification_report(y_test, y_predictions))
